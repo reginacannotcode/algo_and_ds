d325abae95b205e9a731a5eeff4bc111baecd900
reginacannotcode <reginacannotcode@outlook.com>
2023-06-30 12:39:00 +1000

    DataStructure - Linked List Insert Operations

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class LinkedList {
     public Node head;
 
@@ -22,6 +24,53 @@ public class LinkedList {
             curr = curr.next;
         }
         return strB.toString();
+    }
+
+    public LinkedList append(int value) {
+        if (head == null) {
+            head = new Node(value);
+            return this;
+        }
+        var curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = new Node(value);
+        return this;
+    }
+
+    public LinkedList prepend(int value) {
+        var newHead = new Node(value);
+        newHead.next = head;
+        head = newHead;
+        return this;
+    }
+
+    public LinkedList insertAtPos(int pos, int value) throws Exception {
+        if (pos < 0) {
+            throw new Exception("Insert position out of range");
+        }
+        if (pos == 0) {
+            prepend(value);
+        }
+        if (head == null) {
+            throw new Exception("Insert position out of range");
+        }
+        var prev = head;
+        var curr = head.next;
+        var i = 1;
+        while (i < pos) {
+            if (curr == null) {
+                throw new Exception("Insert position out of range");
+            }
+            prev = curr;
+            curr = curr.next;
+            i++;
+        }
+        var newNode = new Node(value);
+        newNode.next = curr;
+        prev.next = newNode;
+        return this;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        var node0 = new Node(1);
-        var node1 = new Node(2);
-        var node2 = new Node(3);
-        var node3 = new Node(4);
-        var node4 = new Node(5);
-
-        node0.next = node1;
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-
-        var list = new LinkedList(node0);
-        System.out.println(list);
+        var list = new LinkedList(1);
+        list.append(2).append(3).append(4).append(5).append(6);
+        System.out.println(list); // 1 -> 2 -> 3 -> 4 -> 5 -> 6
+        list.prepend(-1).prepend(-2);
+        System.out.println(list); // -2 -> -1 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6
+        try {
+            list.insertAtPos(2, 0);
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println(list); // -2 -> -1 -> 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6
+        }
     }
 }
